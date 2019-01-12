932afb431aa3d5ba1b39969872d4e1d36852dca1
liuyukun <liuyukun@syudao.com>
2019-01-12 23:45:18 +0800

    新增了搜索框的一些功能

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,191 +1,187 @@
 <template>
   <div>
-    <div v-if="buycarDate.length == 0">空</div>
-    <div v-else class="goods-wrap">
-      <ul  class='goods-list-menu'>
-        <li class='goods-list' v-for="(item,index) in buycarDate" :key="item.goodsId">
-          <input type="checkbox" class="checked-button" :checked="readyPayGoods.indexOf(item) != -1" @click="checkOne(index,item)"><span>选中</span>
-          <p class='goods-name'>{{ item.goodsname }}</p>
-          <p class='goods-author'>{{ item.goodsauthor }}</p>
-          <p> {{ item.goodsprice }}</p>
-          <div class="count-btn-wrap">
-            <span class="addGoods btn-span" @click="relCount(index)">-</span>
-            <input class="goods-count" type="number" style="width:50px;textAlign:center" v-model="item.goodscount" readonly>
-            <span class="delGoods btn-span" @click="addCount(index)">+</span>
-          </div>
-          <p>单品总价： {{goodsTotal(item.goodsprice,item.goodscount)}}元</p>
-          <span class="delete-span" @click="deleteGoods(index,item)" :data-set="index">删除</span>
-        </li>
-      </ul>
-      <footer>
-        <input type="checkbox" :checked="readyPayGoods.length == buycarDate.length" @click="checkAll"><span>全选</span>
-        <p>所选商品总数： {{ getGoodsAllCount }}件</p>
-        <p>所选商品总价： {{ getTotal }}元</p>
-        <p v-for="g in readyPayGoods" :key="g.id">
-          <span>已勾选商品 {{ g.goodsname}} 数量: {{ g.goodscount }}</span>
-        </p>
-      </footer>
-
+    <div class="search-wrap">
+      <div>
+        <input type="text" @click="clearSearch" v-model="searchmsg" placeholder="请输入您搜索的内容">
+        <button class="search-btn" @click="search">搜索</button>
+      </div>
     </div>
+    <ul v-if="showGoods.length != 0" class='goods-list-menu'>
+      <li class='goods-list' v-for="(item,index) in showGoods" :key="index" :data-set="index">
+        <p class='goods-name'>{{ item.name }}</p>
+        <img class='goods-cover' :src=item.cover alt="">
+        <p class='goods-author'>{{ item.author }}</p>
+        <p> {{ item.price }} <span>元</span> </p>
+        <button class="add-goods-buycar" @click="addBuycar">添加商品到购物车</button>
+      </li>
+    </ul>
+    <div v-else-if="nogoods" class="null-goods-wrap">
+      暂时没有您搜索的内容!
+    </div>
+    <button class="to-goods-buycar" @click="toBuycar">跳转到购物车</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'buycar',
+  name: 'home',
+  path: '/home',
   data () {
     return {
-      buycarDate: [],
-      readyPayGoods: [],
-      goodsAllCount: null,
-      Total: null,
-      checkbox: 0,
-      isCheckedAll: false
+      allGoods: [],
+      showGoods: [],
+      addsGoods: [],
+      searchGoods: [],
+      searchmsg: '',
+      nogoods: false
     }
   },
-  methods : {
-    // 删除
-    deleteGoods (idx,item) {
-      let a = confirm('确认删除？')
-      let newcheckbox = []
-      if (a) {
-        this.buycarDate.splice(idx,1)
-        if (this.readyPayGoods.indexOf(item) != -1) {
-          this.readyPayGoods.splice(this.readyPayGoods.indexOf(item),1)
-        }
-        // 判断点击未选中的货物删除之后如何重置待付款货物 ！！！
-        // localStorage.setItem('Buycar-list',JSON.stringify(this.buycarDate))
-      }
+  methods: {
+    toBuycar () {
+      this.$router.push({path:'/buycar'})
     },
-    addCount (n) {
-      this.buycarDate[n].goodscount ++
-      // 若库存有限 设定上限值
-    },
-    relCount (n) {
-      // 如果只有一件减冬瓜皮
-      if (this.buycarDate[n].goodscount == 1) {
-        return
-      }
-      this.buycarDate[n].goodscount --
-    },
-    goodsTotal (n,m) {
-      let price = parseFloat(n.slice(0,-1))
-      let total = (price * parseFloat(m))
-      return total.toFixed(2)
-    },
-    // 全选
-    checkAll () {
-      // 如果全部单选框都没有选中
-      if (this.isCheckedAll == false) {
-        this.isCheckedAll = true
-        this.readyPayGoods = []
-        this.checkbox = []
-        // 防止数组同源
-        this.readyPayGoods = [...this.buycarDate]
+    addBuycar (e) {
+      let ishas = false
+      if (this.addsGoods.length == 0) {
+         this.addsGoods.push({
+            goodsId: e.path[1].dataset.set,
+            goodsname: e.path[1].children[0].innerText,
+            goodsauthor: e.path[1].children[2].innerText,
+            goodsprice: e.path[1].children[3].innerText,
+            goodscount: 1
+         })
+          alert('添加成功！')
+          localStorage.setItem('Buycar-list',JSON.stringify(this.addsGoods))
       } else {
-        this.isCheckedAll = false
-        this.readyPayGoods = []
-        this.checkbox = 0
-      }
-    },
-    // 单选
-    checkOne (idx,item) {
-      // 只要indexOf的值不是-1 说明数组中已经有某个东西 便于操作
-      if (this.readyPayGoods.indexOf(item) == -1) {
-        this.checkbox = this.checkbox+1
-        this.readyPayGoods.push(this.buycarDate[idx])
-        if (this.checkbox == this.buycarDate.length) {
-          this.isCheckedAll = true
+        this.addsGoods.forEach(g => {
+          if (g.goodsId == e.path[1].dataset.set) {
+            alert('该物品已加入购物车！') 
+            ishas = true
+          }
+        })
+        if (!ishas) {
+          this.addsGoods.push({
+            goodsId: e.path[1].dataset.set,
+            goodsname: e.path[1].children[0].innerText,
+            goodsauthor: e.path[1].children[2].innerText,
+            goodsprice: e.path[1].children[3].innerText,
+            goodscount: 1
+         })
+         alert('添加成功！')
+          localStorage.setItem('Buycar-list',JSON.stringify(this.addsGoods))
         }
-      } else {
-        this.checkbox = this.checkbox - 1
-        // 匹配到待付款物品的下标，进行操作
-        this.readyPayGoods.splice(this.readyPayGoods.indexOf(item),1)
-        this.isCheckedAll = false
       }
-    }
-  },
-  computed : {
-    getGoodsAllCount () {
-      let allCounts = 0
-      this.readyPayGoods.forEach(g => {
-        allCounts += g.goodscount
-      })
-      return allCounts
     },
-    getTotal () {
-      let price = 0
-      this.readyPayGoods.forEach(g => {
-        price += parseFloat(g.goodscount) * parseFloat(g.goodsprice.slice(0,-1))
-      })
-      return price.toFixed(2)
+    search () {
+      if (this.searchmsg.length != 0) {
+        this.searchGoods = []
+        // 获取输入值并将它和所有物品名都大写化（便于检索不区分字母大小写）
+        let searchString = this.searchmsg
+        this.allGoods.forEach(g => {
+          let reg = g.name.toUpperCase()
+          // 全部输入字母变为大写
+          searchString = searchString.toUpperCase()
+          // console.log(reg)
+          // console.log(searchString)
+          if ( reg.indexOf(searchString) != -1) {
+            this.searchGoods.push(g)
+          } else {this.nogoods = true}
+       })
+        this.showGoods = [...this.searchGoods]
+      } else if (this.searchmsg.length == 0) {
+        this.showGoods = [...this.allGoods]
+      }
+      console.log(this.showGoods.length)
+    },
+    clearSearch () {
+      this.searchGoods = []
     }
   },
   created () {
-    // 实际操作应该也是获取用户的个人信息一类api
-    if (localStorage.getItem('Buycar-list') == null) {
-      this.buycarDate = []
+    this.axios.get('https://www.easy-mock.com/mock/5c1b0deb48952b7bd6514342/example/lykmobileshopAppdata')
+      .then((response) => {
+      this.allGoods = response.data
+      this.showGoods = [...this.allGoods]
+      }).catch(()=>{
+      console.log('加载失败')
+    })
+    if (localStorage.getItem('Buycar-list') === null) {
+      this.addsGoods = []
     } else {
-        this.buycarDate = JSON.parse(localStorage.getItem('Buycar-list'))
+      this.addsGoods = JSON.parse(localStorage.getItem('Buycar-list'))
     }
   }
 }
 </script>
 
-<style lang="less" scoped>
-div {
-  text-align: left;
-}
-.goods-list {
-  position: relative;
-  padding: 10px;
-  width: 500px;
-  .goods-name {
-    color:orange;
-  }
-  .count-btn-wrap {
-    display: flex;
-      .goods-count {
-        position: relative;
-        display: block;
-        height: 30px;
+<style lang="less" scope>
+  .search-wrap {
+    position: relative;
+    width: 300px;
+    height: 80px;
+    text-align: left;
+    line-height: 80px;
+    border:1px solid #ddd;
+    border-radius: 5px;
+    background: #fff;
+    input {
+      width: 200px;
+      height: 60px;
+      padding-left:10px;
+      letter-spacing: 2px;
+      border:none;
+      outline:none;
     }
-      .btn-span {
-        display: block;
-        width: 30px;
-        height: 30px;
-        line-height: 30px;
-        text-align: center;
-        border: 1px solid black;
+    .search-btn {
+      position: absolute;
+      width: 100px;
+      height: 78px;
+      right:0;
+      border:none;
+      outline: none;
+      font-size: 20px;
+      color:#fff;
+      background: rgb(230, 139, 20);
+      cursor: pointer;
+      border-top-right-radius: 5px;
+      border-bottom-right-radius: 5px;
+    }
+    .search-btn:hover {
+      background: rgb(139, 139, 240);
     }
   }
-
-  .delete-span {
-    position: absolute;
-    display: block;
-    width: 80px;
-    height: 40px;
-    line-height: 40px;
-    right: 10px;
-    top:50%;
-    transform: translateY(-50%);
-    z-index: 10;
-    text-align: center;
-    color:#fff;
-    background: red;
-    cursor: pointer;
+  .goods-list-menu {
+    text-align: left;
+    list-style: none;
+    p {
+      width: 300px;
+    }
+    .goods-list {
+      padding: 5px;
+      .add-goods-buycar {
+        height: 60px;
+        width: 200px;
+        bottom:0;
+        border:none;
+        outline: none;
+        background: rgb(243, 129, 35);
+      }      
+      .goods-name {
+        font-size: 20px;
+        font-weight: bold;
+      }
+      .goods-cover {
+        width: 100px;
+      }
+    }
   }
-}
-footer {
-  position: fixed;
-  width: 100%;
-  bottom:0;
-  background: pink;
-  z-index: 11;
-}
-ul {
-  margin-bottom:500px;
-}
+  .to-goods-buycar {
+    position: fixed;
+    height: 60px;
+    width: 300px;
+    bottom:0;
+    border:none;
+    outline: none;
+    background: lightblue;
+  }
 </style>
-
-
